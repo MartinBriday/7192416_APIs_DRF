a5977f8f55ee84455ae0a51cf3ac2ab5a5a3f949
MartinBriday <briday.martin@gmail.com>
2023-03-30 12:24:22 +0200

    P1C4 Minor change in urls using router

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
 from shop.views import CategoryAPIView, ProductAPIView
+
+router = routers.SimpleRouter()
+
+router.register("category", CategoryAPIView, basename="category")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/category/', CategoryAPIView.as_view()),
-    path('api/product/', ProductAPIView.as_view())
+    # path('api/category/', CategoryAPIView.as_view()),
+    # path('api/product/', ProductAPIView.as_view())
+    path("api/", include(router.urls))
 ]
